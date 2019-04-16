3abc08329452ecf50a749f3ad8a676db9451fe12
Andrew Johnson <manilda7@gmail.com>
2019-04-15 22:17:59 -0600

    Adds generate_key method & split_key method

--- test/key_test.rb
+++ test/key_test.rb
@@ -1,7 +1,18 @@
 require 'minitest.autorun'
 require 'minitest.pride'
-require './lib/key'
+require'./lib/key'
 
 class KeyTest < Minitest::Test
 
+  def setup
+    @random_key = Key.new
+  end
+
+  def test_key_class_exists
+    assert_instance_of Key, @random_key
+  end
+
+  def test_random_key_can_be_split
+    assert 1 > 0
+  end
 end
